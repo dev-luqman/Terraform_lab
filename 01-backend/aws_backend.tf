56f223318c793b93a17ee463cb93905abb1bde20
balogun Lukman <lukheebalo@gmail.com>
2023-04-24 23:22:29 +0100

    Hot Fix

--- 01-backend/aws_backend.tf
+++ 01-backend/aws_backend.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "0.13.5"
+  required_version = ">=1.2.3"
   #############################################################
   ## AFTER RUNNING TERRAFORM APPLY (WITH LOCAL BACKEND)
   ## YOU WILL UNCOMMENT THIS CODE THEN RERUN TERRAFORM INIT
