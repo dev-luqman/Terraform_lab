5217a1b464f9001284699acc37614f7fd217c957
balogun Lukman <lukheebalo@gmail.com>
2023-04-24 23:30:40 +0100

    version change

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
   #   region         = "us-east-1"
   #   dynamodb_table = "terraform-state-locking"
   #   encrypt        = true
-  # }required_version = ">=1.2.3"
-  required_version = ">=1.2.3"
+  # }required_version = "~>1.2.3"
+  required_version = "~>1.2.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
