33275550bf75a47f33d6592a7c70bc537b188c4f
balogun Lukman <lukheebalo@gmail.com>
2023-04-24 19:17:15 +0100

    Hot fix

--- main.tf
+++ main.tf
@@ -26,5 +26,5 @@ provider "aws" {
 
 resource "aws_instance" "example" {
   ami           = "ami-007855ac798b5175e" # Ubuntu 20.04 LTS // us-east-1 // Pleaase Change accordingly
-  instance_type = "t2.micro"
+  instance_type = "tX.mEcro"
 }
